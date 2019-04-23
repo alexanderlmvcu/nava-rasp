5e20ba14ab0a6754ae87158fd25ee968eab420de
alexanderlmvcu <alexanderlm@vcu.edu>
2019-04-23 18:04:50 -0400

    working code as barebones

---
+++ nava-server/script.py
@@ -0,0 +1,100 @@
+#!/usr/bin/python
+import RPi.GPIO as GPIO # Library for Raspberry Pi 
+import time
+import random
+from random import randint
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(2,GPIO.IN) 
+GPIO.setup(3, GPIO.IN)
+
+# Initializing variables that will be used to determine when the sensors have changed states from ON/OFF
+S1 = GPIO.input(2)
+S2 = GPIO.input(3)
+
+tick = 0
+
+Flag0 = False
+Flag1 = True
+FlagON = True
+FlagDirection = True
+value = randint(7, 11)
+
+while True:
+
+    while Flag0 == False: # Makes sure that Sensor 1 and Sensor2 are both zero 
+        if S1 == 0 and S2 == 0: ## Flag becomes "True" so it exists the while loop, reads S1 and S2, and enters into the next loop
+            Flag0 = True
+            Flag1 = False
+            S1 = GPIO.input(2)
+            S2 = GPIO.input(3)
+        else: # reads S1 and S2 until Flag0 becomes True
+            S1 = GPIO.input(2)
+            S2 = GPIO.input(3)
+
+    while Flag1 == False:
+        if S1 == True and S2 == False:  # Sensor 1 is on and Sensor 2 is off 
+            Flag1 = True  
+            FlagON = False
+            S1 = GPIO.input(2)
+            S2 = GPIO.input(3)
+
+        if S1 == False and S2 == True:  # Sensor 1 is off and Sensor 2 is on
+            Flag1 = True
+            FlagON = False
+            S1 = GPIO.input(2)
+            S2 = GPIO.input(3)
+
+        else: # keeps reading until one sensor changes state
+            S1 = GPIO.input(2)
+            S2 = GPIO.input(3)
+    
+    # Forward direction loop
+    while FlagON == False:
+        if S1 == True and S2 == True: # Sensor 1 is on and Sensor 2 is on
+            FlagON = True
+            FlagDirection = False 
+            S1 = GPIO.input(2)
+            S2 = GPIO.input(3)
+       
+        else: # Keep reading until Sensor 1 is on and Sensor 2 is on
+            S1 = GPIO.input(2)
+            S2 = GPIO.input(3)
+    
+    # Direction Loop
+    while FlagDirection == False:
+
+        # FORWARD DIRECTION
+        if S1 == False and S2 == True:  # Sensor 1 is off and Sensor 2 is on
+            FlagDirection = True
+            Flag0 = False
+            S1 = GPIO.input(2)
+            S2 = GPIO.input(3)
+            tick += 1
+            print("Forward")
+            print(tick)
+        
+        # BACKWARD DIRECTION
+        if S1 == True and S2 == False:
+            FlagDirection = True
+            Flag0 = False
+            S1 = GPIO.input(2)
+            S2 = GPIO.input(3)
+            tick += -1
+            print("Backward")
+            print(tick)
+
+        else: # Keep reading until the sense of direction is the same
+            S1 = GPIO.input(2)
+            S2 = GPIO.input(3)
+    
+    #Determing the "sweet spot"
+   # if tick < value:
+        #print("Too low")
+
+    #if (tick > value):
+        #print("Too high")
+
+    #else:
+        #print("perfect")
---
+++ nava-server/server.js
@@ -0,0 +1,52 @@
+
+const path = require('path')
+const {spawn} = require('child_process')
+const express = require('express')
+const app = express()
+const http = require("http")
+const WebSocket = require("ws")
+const server = http.createServer(app);
+const wss = new WebSocket.Server({server});
+
+function runScript() {
+  return spawn('python', [
+    "-u",
+    path.join(__dirname, 'script.py')
+  ]);
+}
+
+
+app.use('/public', express.static(path.join(__dirname, 'public')))
+//may need to also be change to /public, TBD YUP needed to do it
+app.get('/public', function (req, res) {
+  res.sendFile(path.join(__dirname, 'index.html'))
+})
+//if running into problems, try making this '/public' instead 
+
+
+function runScriptInWebsocket(id, ws) {
+  const child = runScript("foobar")
+  child.stdout.on('data', (data) => {
+    ws.send(`${id}:${data}`);
+  });
+  child.stderr.on('data', (data) => {
+    ws.send(`${id}:error:\n${data}`);
+  });
+  child.stderr.on('close', () => {
+    ws.send(`${id}:done`);
+  });
+}
+
+let id = 1
+wss.on('connection', (ws) => {
+  const thisId = id++;
+  ws.on('message', (message) => {
+    ws.send(`You sent -> ${message}`);
+    if ("run" === message) {
+      runScriptInWebsocket(thisId, ws)
+    }
+  });
+  ws.send('Connection with WebSocket server initialized');
+});
+
+server.listen(8080, () => console.log('Server running on port 8080'))
